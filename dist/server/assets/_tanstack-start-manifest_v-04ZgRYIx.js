1bd0f73616ed7a88fff51dde730ecce08911df75
Nour <contac.savistas@gmail.com>
2026-06-07 21:05:44 +0200

    fix router entry

--- dist/server/assets/_tanstack-start-manifest_v-04ZgRYIx.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Ounis/taknaacode/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-B604XI1d.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-B604XI1d.js" } }] }, "/": { filePath: "C:/Users/Ounis/taknaacode/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-DSO828qm.js"] } } });
-export {
-  tsrStartManifest
-};
