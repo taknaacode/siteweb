156ea36f209ac88c7df883eee4ff7cb330787367
Nour <contac.savistas@gmail.com>
2026-06-09 12:19:51 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import "./i18n";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
-import "./i18n";
 import "./styles.css";
 
 const router = getRouter();
